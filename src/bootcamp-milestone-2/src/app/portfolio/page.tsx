ed638b623ec115857cd8b460363249d79555e4a8
srishmaulik <srishmaulik2004@gmail.com>
2024-01-22 22:44:20 -0800

    portfolio

--- src/bootcamp-milestone-2/src/app/portfolio/page.tsx
+++ src/bootcamp-milestone-2/src/app/portfolio/page.tsx
@@ -1,38 +1,43 @@
+// portfolio.js
 import React from "react";
 import Link from "next/link";
-import style from "./portfolio.module.css";
+import styles from "./portfolio.module.css";
 
-export default function Portfolio() {
-    return (
-        <header className={style.portfolio}>
-            <main>
-                <h1 className="page-title">Masai Mara, Kenya</h1>
+const Portfolio = () => {
+  return (
+    <header className={styles.portfolio}>
+      <main>
+        <h1 className="page-title">Masai Mara, Kenya</h1>
 
-                <Link href="index.html">
-                    <div>
-                        <img src="IMG_5942.jpg" alt="Project Image" width="400" height="600" />
-                    </div>
-                </Link>
+        <Link href="/index.html">
+          <a>
+            <div>
+              <img src="IMG_5942.jpg" alt="Project Image" width="400" height="600" />
+            </div>
+          </a>
+        </Link>
 
-                <div className="project-details">
-                    <p className="project-description">
-                        Encountering the African Lion, I overcame the telephoto limitations of my iPhone XR by positioning it behind my binoculars. The results were truly immaculate!<br /> As a creative touch, I've included this just for fun. This is the first website I have ever made.
+        <div className={styles.projectDetails}>
+          <p className={styles.projectDescription}>
+            Encountering the African Lion, I overcame the telephoto limitations of my iPhone XR by positioning it behind
+            my binoculars. The results were truly immaculate!<br /> As a creative touch, I've included this just for fun.
+            This is the first website I have ever made.
 
-                        <Link href="resume.html">
-                            <div>
-                                Learn More about me...
-                            </div>
-                        </Link>
-                    </p>
-                </div>
+            <Link href="/resume.html">
+              <a>Learn More about me...</a>
+            </Link>
+          </p>
+        </div>
 
-                <div className="about">
-                    <div className="about-image"></div>
-                    <div className="about-text"></div>
-                </div>
+        <div className={styles.about}>
+          <div className={styles.aboutImage}></div>
+          <div className={styles.aboutText}></div>
+        </div>
 
-                <footer className="footer">© 2023 My Personal Website | All Rights Reserved </footer>
-            </main>
-        </header>
-    );
-}
+        <footer className={styles.footer}>© 2023 My Personal Website | All Rights Reserved </footer>
+      </main>
+    </header>
+  );
+};
+
+export default Portfolio;
