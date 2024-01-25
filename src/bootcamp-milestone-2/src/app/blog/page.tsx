ad80ea31f5fcab7f9caf3908650ead51d6debcbf
srishmaulik <srishmaulik2004@gmail.com>
2024-01-25 01:14:32 -0800

    added mongoose for blogs

--- src/bootcamp-milestone-2/src/app/blog/page.tsx
+++ src/bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,16 +1,44 @@
 import React from "react";
-import BlogPreview from "@/components/blogPreview";
-import blogs from "../blogData";
+import BlogPreview from '@/components/blogPreview';
+import Blogs from "@/database/blogSchema";
+import connectDB from "@/database/db";
 
-export default function Blog() {
-  return (
-    <>
-      <div className="blog-container"></div>
-      {blogs.map(
-        (blog) => (
-          <BlogPreview key={blog.title} {...blog} />
-        ) // This is how we call the component
-      )}
-    </>
-  );
+
+async function getBlogs()
+{
+	await connectDB() // function from db.ts before
+
+	try {
+			// query for all blogs and sort by date
+	    const blogs = await Blogs.find().sort({ date: -1 }).orFail()
+			// send a response as the blogs as the message
+            //console.log("hello " + blogs)
+	    return blogs
+	} catch (err) 
+    {
+        return null
+	}
 }
+
+export default async function Blog(){
+    const blogs = await getBlogs();
+    if (blogs) {
+    return (
+    <main>
+        <h2 className="page-title">Blogs</h2>
+        <div>
+            {blogs?.map(blog => 
+                <BlogPreview  
+                title={blog.title}
+                slug = {blog.slug}
+                date = {(blog.date)}
+                description={blog.description}
+        
+               
+               
+                />
+		    )}
+        </div>
+    </main>
+    )};
+    };
--- src/bootcamp-milestone-2/src/app/blogData.ts
+++ src/bootcamp-milestone-2/src/app/blogData.ts
@@ -1,28 +1,35 @@
 // src/app/blogData.ts
 
+import Blog from "@/database/blogSchema";
+import connectDB from "@/database/db";
+
 export interface Blog {
     title: string;
+    slug: string;
     date: string;
     description: string;
-    slug: string;
+    
     // Add more properties as needed
   }
   
   const blogs: Blog[] = [
     {
       title: "Blog Post 1",
+      slug: "blog-1",
       date: "2023-10-22",
       description: "This is the first blog post description.",
-      slug: "blog-1",
+     
     },
     {
       title: "Blog Post 2",
+      slug: "blog-2",
       date: "2023-10-23",
       description: "This is the second blog post description.",
-      slug: "blog-2",
+      
     },
     // Add more blog posts as needed
   ];
   
   export default blogs;
-  
+
+ 
